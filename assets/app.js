6e07a456aeff32d6bae45306add89d690cd2fe78
NoorAhmed786 <aniiahmed380@gmail.com>
2023-05-15 01:47:04 +0500

    items

---
+++ assets/app.js
@@ -0,0 +1,27 @@
+///qstn 1
+var age="I AM 23 yaers old";
+alert(age);
+
+// var newNum=12;
+// num = newNum + 12;
+// alert(num);
+
+//qustn 2
+var count= "you have visited this site 14 times";
+alert( count);
+
+//qustn 3
+ var birth= document.write("My birth year is 1999 <br> Data type of my declared number <br> ");
+
+//qustn 4
+var customerName="aslam";
+var title="pents";
+var quantity=50;
+
+var sum=" "+customerName +" "+"orderds"+" "+ title +" "+ quantity+" "+"on khadi clothing store";
+var print=document.write(sum);
+
+// qustn 5
+
+ 
+
